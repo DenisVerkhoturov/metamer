70ca31da45b3e225640cf51cbf4927d883f78164
alex-klimina <alex.klimina@yandex.ru>
2019-03-19 00:24:07 +0300

    made issue for codacy

--- src/main/java/alkli/hello/HelloWorld.java
+++ src/main/java/alkli/hello/HelloWorld.java
@@ -2,6 +2,8 @@ package alkli.hello;
 
 public class HelloWorld {
 
+    String badScope = "I'm not private";
+
     public static void main(String[] args) {
         System.out.println("Hello Gradle!");
     }
