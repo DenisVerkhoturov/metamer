a4fff8b69a6f67520cb723123aa9c663593f7ceb
Denis Verkhoturov <mod.satyr@gmail.com>
2019-04-27 16:12:45 +0300

    chore: Update PATH after choco installation
    Signed-off-by: Denis Verkhoturov <mod.satyr@gmail.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,5 +5,9 @@ if [[ ${TRAVIS_OS_NAME} == 'windows' ]]; then
     for package in ${CHOCO_PACKAGES//,/ }; do
         choco install ${package}
     done
-    refreshenv
+
+    export PATH="$(powershell -Command '("Process", "Machine" | % {
+        [Environment]::GetEnvironmentVariable("PATH", $_) -Split ";" -Replace "\\$", ""
+    } | Select -Unique | % { cygpath $_ }) -Join ":"')"
+    echo "PATH=${PATH}"
 fi
