2815e9687829d9479379d714770b0faa8691f753
Denis Verkhoturov <mod.satyr@gmail.com>
2019-04-27 15:56:19 +0300

    chore: Fix choco installation script
    Signed-off-by: Denis Verkhoturov <mod.satyr@gmail.com>

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ ${TRAVIS_OS_NAME} == 'windows' ]]; then
-    for package in $(CHOCO_PACKAGES//,/); do
+    for package in ${CHOCO_PACKAGES//,/ }; do
         choco install ${package}
     done
     refreshenv
