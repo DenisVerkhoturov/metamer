c2a42efab9c28f356c7663c9eb1b858b3ffc206c
Shapovalova <irishapovalova13@gmail.com>
2019-05-24 15:37:07 +0300

    test: Delete resource files

--- cli/src/main/java/metamer/utils/Paths.java
+++ cli/src/main/java/metamer/utils/Paths.java
@@ -25,7 +25,6 @@
 
 package metamer.utils;
 
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 
 public class Paths {
@@ -36,13 +35,5 @@ public class Paths {
             return "";
         }
         return s.substring(index + 1);
-    }
-
-    public static Path resource(final String filename) throws URISyntaxException {
-        return resource(Paths.class, filename);
-    }
-
-    public static Path resource(final Class clazz, final String filename) throws URISyntaxException {
-        return java.nio.file.Paths.get(clazz.getResource(filename).toURI());
     }
 }
