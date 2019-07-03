701b751268a233aa47aad1593dc68cc16c34a6b5
AnBrusn <brusnitsyna.an@yandex.ru>
2019-07-03 13:55:55 +0300

    Refactor : change some methods in Graph class

--- assembler/src/test/java/metamer/graph/GraphTest.java
+++ assembler/src/test/java/metamer/graph/GraphTest.java
@@ -27,34 +27,35 @@ package metamer.graph;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Stream;
 
+import static metamer.graph.Graph.graph;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
 
 
 class GraphTest {
     @Test
     @DisplayName("all nodes should present when create graph")
     public void testIfAllNodesArePresent() {
-        Graph gr = new Graph(new HashMap<>(), new HashMap<>(), 3);
-        gr.createFromStream(Stream.of("AATR", "ART"));
-        assertThat(gr.getNodes(), hasKey("AA"));
-        assertThat(gr.getNodes(), hasKey("AT"));
-        assertThat(gr.getNodes(), hasKey("AR"));
-        assertThat(gr.getNodes(), hasKey("TR"));
-        assertThat(gr.getNodes(), hasKey("RT"));
+        Graph gr = graph(3, Stream.of("AATR", "ART"));
+        assertThat(gr.nodes(), containsInAnyOrder(
+                new Node("AA"), new Node("AT"), new Node("AR"), new Node("TR"), new Node("RT")
+        ));
     }
 
     @Test
     @DisplayName("nodes should be connected when there is only one way to connect them")
     public void testOptimization() {
-        Graph gr = Graph.of(
+        Graph gr = graph(
                 3,
                 Map.entry(new Node("AT"), new Node("TG")),
                 Map.entry(new Node("TG"), new Node("GG")),
@@ -66,37 +67,46 @@ class GraphTest {
                 Map.entry(new Node("GC"), new Node("CA"))
         );
         Graph optGr = gr.optimizeGraph();
-        GraphCycle t = new GraphCycle(optGr, 3);
-        assertThat(optGr.getNodes(), hasKey("AT"));
-        assertThat(optGr.getNodes(), hasKey("TG"));
-        assertThat(optGr.getNodes(), hasKey("GG"));
-        assertThat(optGr.getNodes(), hasKey("GC"));
-        assertThat(optGr.getNodes(), hasKey("CGT"));
-        assertThat(optGr.getNodes(), hasKey("CA"));
+        assertThat(optGr.nodes(), containsInAnyOrder(
+                new Node("AT"), new Node("TG"), new Node("GC"),
+                new Node("GG"), new Node("CGT"), new Node("CA")
+        ));
     }
 
     @Test
     @DisplayName("node should not present if there is no way to connect parts")
     public void testIfNodeNotPresent() {
-        Graph gr = new Graph(new HashMap<>(), new HashMap<>(), 3);
-        gr.createFromStream(Stream.of("AAA", "TTT"));
-        assertThat(gr.getNodes(), not(hasKey("AT")));
+        Graph gr = graph(3, Stream.of("AAA", "TTT"));
+        assertThat(gr.nodes(), not(contains(new Node("AT"))));
     }
 
     @Test
     @DisplayName("node should have information about all neighbors when graph created")
     public void testIfEdgeContainsAllNodes() {
-        Graph gr = new Graph(new HashMap<>(), new HashMap<>(), 3);
-        gr.createFromStream(Stream.of("AARAT", "AAT"));
-        assertThat(gr.getNeighbors().get(gr.getNodes().get("AA")), hasSize(3));
+        Graph gr = graph(3, Stream.of("AARAT", "AAT"));
+        Node tmp = new Node("AA");
+        assertThat(gr.getNeighbors().get(gr.nodes().stream()
+                .filter(data -> Objects.equals(data, tmp)).findFirst().get()), hasSize(3));
     }
 
     @Test
     @DisplayName("kmer field should be initializes correctly when graph created")
     public void testCorrectnessOfNodeKmerField() {
-        Graph gr = new Graph(new HashMap<>(), new HashMap<>(), 3);
-        gr.createFromStream(Stream.of("AARAT", "AAT"));
-        assertThat(gr.getNodes().get("AA").kmer, equalTo("AA"));
+        Graph gr = graph(3, Stream.of("AARAT", "AAT"));
+        Node tmp = new Node("AA");
+        assertThat(gr.nodes().stream().filter(data -> Objects.equals(data, tmp))
+                .findFirst().get().kmer, equalTo("AA"));
     }
 
+    @Test
+    @DisplayName("graph should contain all edges when method called")
+    public void testIfAllEdgesPresent() {
+        Graph gr = graph(3, Stream.of("ARAT"));
+        Set<Edge> edges = gr.edges();
+        assertThat(gr.edges(), containsInAnyOrder(
+                new Edge(new Node("AR"), new Node("RA"), 3),
+                new Edge(new Node("RA"), new Node("AT"), 3),
+                new Edge(new Node("RA"), new Node("AR"), 3)
+        ));
+    }
 }
